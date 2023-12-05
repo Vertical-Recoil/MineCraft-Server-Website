f6cac7a28e0cd3c02684d1e13afdb3a5e64d3470
Amiran Ropyak <43885921+Vertical-Recoil@users.noreply.github.com>
2023-12-04 19:24:15 -0500

    Stable 2.0.0
    Signed-off-by: Amiran Ropyak <43885921+Vertical-Recoil@users.noreply.github.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: deprecated_member_use
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -12,52 +10,183 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(
-            255, 119, 119, 119), // Set the background color for the entire page
-        body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 156, 156,
-                  156), // Set the background color for the box with text
-              borderRadius: BorderRadius.circular(20),
+        backgroundColor: const Color(0xFF777777),
+        appBar: AppBar(
+          backgroundColor: Colors.green[900],
+          title: const Text(
+            'VerticalRecoil\'s Minecraft Assets',
+            style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text(
-                  'SpookierCraft 1.20.1',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+          ),
+          centerTitle: true,
+          toolbarHeight: 80,
+        ),
+        body: Align(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 500,
+                height: 100,
+                margin: const EdgeInsets.only(bottom: 20),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF9C9C9C),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: const Center(
+                  child: Text(
+                    'The version shown as latest is required if you wish to join the server',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 116, 0, 0),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
                   ),
                 ),
-                const SizedBox(height: 10),
-                const Text(
-                  'Latest Modpack Version ',
-                  style: TextStyle(fontSize: 16),
+              ),
+              Container(
+                width: 500,
+                height: 300,
+                margin: const EdgeInsets.only(bottom: 20),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF9C9C9C),
+                  borderRadius: BorderRadius.circular(20),
                 ),
-                const Text(
-                  '2.2.0',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 175, 76, 76),
-                    fontWeight: FontWeight.bold,
-                  ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'SpookierCraft 1.20.1',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    const Text(
+                      'Latest Modpack Version ',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    const Text(
+                      '2.2.0',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 238, 255, 82),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        _launchURL(
+                            'https://drive.google.com/file/d/1HHpWpuqT6RAvJrqWrnmJ2uyc5JKvF7e4/view?usp=sharing');
+                      },
+                      style: ButtonStyle(
+                        fixedSize:
+                            MaterialStateProperty.all(const Size(200, 50)),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.lightGreen),
+                      ),
+                      child: const Text(
+                        'Download',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ],
                 ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    _launchURL(
-                        'https://drive.google.com/file/d/1HHpWpuqT6RAvJrqWrnmJ2uyc5JKvF7e4/view?usp=sharing');
-                  },
-                  style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.lightGreen),
-                  ),
-                  child: const Text('Download'),
+              ),
+              Container(
+                width: 500,
+                height: 275,
+                margin: const EdgeInsets.only(bottom: 20),
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF9C9C9C),
+                  borderRadius: BorderRadius.circular(20),
                 ),
-              ],
-            ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'See other modpacks',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Previous versions as well as\nother modpacks can be found here!',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        _launchURL(
+                            'https://drive.google.com/drive/folders/1qKoDu74IuhV4MwgGEtny69L_eyKsUAms?usp=sharing');
+                      },
+                      style: ButtonStyle(
+                        fixedSize:
+                            MaterialStateProperty.all(const Size(200, 50)),
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.lightGreen),
+                      ),
+                      child: const Text(
+                        'Browse',
+                        style: TextStyle(fontSize: 30),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 300,
+                height: 180,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF9C9C9C),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      'Join our discord',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        _launchURL(
+                            'https://discord.gg/tpArk4fhr8'); // Replace with your Discord link
+                      },
+                      style: ButtonStyle(
+                        fixedSize:
+                            MaterialStateProperty.all(const Size(200, 50)),
+                        backgroundColor: MaterialStateProperty.all(
+                            const Color.fromARGB(255, 0, 95, 173)),
+                      ),
+                      child: const Text(
+                        'Join Now',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
